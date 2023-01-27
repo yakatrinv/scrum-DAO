b42ae330d7ffc679146ebe1db26245e26683a6f3
Oleg.Avdeenko <93432136+Oleg1985Avdeenko@users.noreply.github.com>
2023-01-27 23:54:42 +0300

    edit AppTest

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        Assertions.assertTrue( true );
+    }
+}
